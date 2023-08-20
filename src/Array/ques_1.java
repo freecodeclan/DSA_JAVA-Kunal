dc18e25a6381e2b7533b28cbdfb6b0e09a2ddf34
Harsh <harshmehra53@gmail.com>
2023-08-20 17:43:03 +0530

    New file

--- src/Array/ques_1.java
+++ src/Array/ques_1.java
@@ -1,36 +1,22 @@
+/* Swapping two numbers */
 package Array;
 
 import java.util.Arrays;
 import java.util.Scanner;
 
 
-
 public class ques_1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-//        int[] rollNo = new int[5];
-//        System.out.println("Enter the elements of an array");
-//        for(int i = 0; i<rollNo.length; i++){
-//            rollNo[i] = in.nextInt();
-//        }
-//        System.out.println("Entered array looks like ");
-////        for( int num : rollNo){
-////            System.out.print(num + " ");
-////        }
-//        System.out.println(Arrays.toString(rollNo));
-        String[] str = new String[4];
-        System.out.println("Enter the names of car: ");
-        for(int i=0; i<str.length; i++){
-            str[i] = in.next();
-        }
-        System.out.println("Here the beauties are: ");
-//        System.out.println(Arrays.toString(str));
-        for(String num : str){
-            System.out.print(num + " ");
-            System.out.println("Hello World");
-        }
-        }
+        System.out.println("Enter the first element to be swapped");
+        int a  = in.nextInt();
+        System.out.println("Enter the second element to be swapped");
+        int b = in.nextInt();
 
+        int temp = a;
+        a = b;
+        b = temp;
 
-
-    }
+        System.out.println("a = " + a + " b = " + b);
+        }
+}
