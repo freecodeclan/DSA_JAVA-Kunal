a40dfd3082243f92818de9c9301b70d7f1cb13c2
Harsh <harshmehra53@gmail.com>
2023-08-20 17:56:04 +0530

    new

--- src/Array/ques_2.java
+++ src/Array/ques_2.java
@@ -1,7 +1,24 @@
+/* Swapping an elements in array */
 package Array;
+
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class ques_2 {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Scanner in = new Scanner(System.in);
+        int[] arr = new int[5];
+        System.out.println("Enter the elements into the array: ");
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = in.nextInt();
+        }
+        swap(arr, 1,3);
+        System.out.println(Arrays.toString(arr));
+
+    }
+    static void swap(int[] a, int index1, int index2){
+        int temp = a[index1];
+        a[index1] = a[index2];
+        a[index2] = temp;
     }
 }
