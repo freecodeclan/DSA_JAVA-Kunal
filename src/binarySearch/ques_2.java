f518ec022e29315d6e20d943f6c8c8e56949a3a1
Harsh <harshmehra53@gmail.com>
2023-09-02 18:49:31 +0530

    Update

---
+++ src/binarySearch/ques_2.java
@@ -0,0 +1,49 @@
+/* Find the indexValue of the targeted value using Order-Agnostic Binary Search */
+package binarySearch;
+
+public class ques_2 {
+    public static void main(String[] args) {
+        int[] ar = {5,10,12,15,20,25};
+        int target = 20;
+
+        int ans = orderAgnosticBS(ar, target);
+        System.out.println(ans);
+
+    }
+    static int orderAgnosticBS(int[] arr, int t){
+        int s = 0;
+        int e = arr.length - 1;
+
+        // Finding array is sorted in ascending or descending
+        boolean isAsc;
+        if (arr[s] < arr[e]) isAsc = true;
+        else isAsc = false;
+
+        while(s <= e){
+            // find mid element
+            int mid = s + (e - s) / 2;
+            if(arr[mid] == t){
+                return mid;
+            }
+            if(isAsc){
+                if(t < arr[mid]){
+                    e = mid - 1;
+                }
+                else if(t > arr[mid]){
+                    s = mid + 1;
+                }
+                else{
+                    if(t > arr[mid]){
+                        e = mid - 1;
+                    }
+                    else{
+                        s = mid + 1;
+                    }
+
+                }
+            }
+
+        }
+        return -1;
+    }
+}
