f7a96647acce5471dab1ad345b5a808bfbe080c7
Harsh <harshmehra53@gmail.com>
2023-09-16 08:26:33 +0530

    Update

---
+++ src/Insertion_Sort/ques_1.java
@@ -0,0 +1,31 @@
+/* Sort an array using Insertion Sorting */
+package Insertion_Sort;
+
+import java.util.Arrays;
+
+public class ques_1 {
+    public static void main(String[] args) {
+        int[] arr = {-10, -2, -12, 1, 4, 2};
+        insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+    static void insertionSort(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++){
+            for (int j = i + 1; j > 0; j--){
+                if (arr[j] < arr[j - 1]){
+                    swap(arr, j, j - 1);
+                }
+                else {
+                    break;
+                }
+            }
+        }
+
+    }
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
