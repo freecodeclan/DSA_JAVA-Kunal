7a047571f9817cd0efb2a0fea5ca2657495c3b15
Harsh <harshmehra53@gmail.com>
2023-10-20 10:26:25 +0530

    UPDATE

--- src/Map/MaxFrequency.java
+++ src/Map/MaxFrequency.java
@@ -15,16 +15,27 @@ public class MaxFrequency {
 
         Map<Integer, Integer> freq = new HashMap<>();
 
-        for(int element : arr){
-            if (!freq.containsKey(element)){
-                freq.put(element,1);
+        for(int element : arr){                 //-- 'element' stand for element inside an array
+            if (!freq.containsKey(element)){    //-- Checking whether an element is present or not inside an array
+                freq.put(element,1);            //-- Initialize its frequency to 1
             }
             else {
+                //-- Element is already in the map increment its frequency
                 freq.put(element, freq.get(element) + 1);
             }
             }
         System.out.println("Frequency Map");
         System.out.println(freq.entrySet());
+        int maxFreq = 0;
+        int ansKey = -1;
+
+        for(var e : freq.entrySet()){
+            if (e.getValue() > maxFreq){        //-- Checking if current frequency is greater than maximum frequency
+                maxFreq = e.getValue();
+                ansKey = e.getKey();
+            }
+        }
+        System.out.printf("%d has max frequency and it occurs %d times", ansKey, maxFreq);
         }
     }
 
